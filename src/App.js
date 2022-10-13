ea4ebb0bf373270629ea1ffb0e1c60384b34a97e
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-10-13 10:07:46 -0400

    Configure redux, add redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import MainPage from "./pages/MainPage";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
       <MainPage />
-    </div>
+    </Provider>
   );
 }
 
