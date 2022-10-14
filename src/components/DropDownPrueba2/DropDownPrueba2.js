3d66e1072678550270bdb82551bd3965f74a61c7
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-10-14 19:18:28 -0400

    configure dropdown2

--- src/components/DropDownPrueba2/DropDownPrueba2.js
+++ src/components/DropDownPrueba2/DropDownPrueba2.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import {
+  Avatar,
   //Avatar,
   Box,
   Checkbox,
@@ -7,7 +8,6 @@ import {
   FormControl,
   FormControlLabel,
   FormGroup,
-  FormLabel,
   List,
   ListItem,
   ListItemButton,
@@ -18,7 +18,7 @@ import {
 //import { useDispatch } from "react-redux";
 //import Icono1 from "../../assets/Dropdown/Icono1.jpg";
 //import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
-//import { ok } from "../../assets/Dropdown/ok.jpg";
+import newIcon from "../../assets/Dropdown/newIcon.jpg";
 
 const array = ["Family", "Work Friends", "Another Label"];
 
@@ -37,53 +37,60 @@ const DropDownPrueba = () => {
         background: "#F5F8FA",
         border: "1px solid #CBD6E2",
         borderRadius: "2px",
+        display: "flex",
+        flexWrap: "wrap",
       }}
     >
-      <List>
-        <ListItem>
-          <ListItemButton>
-            <Box>{"<"}</Box>
-            <Box>
-              <ListItemText primary={"All contacts"} />
-            </Box>
+      {/* <Box sx={{ width: "15.55", height: "11.65px" }}>{"<"}</Box> */}
+      <Avatar
+        sx={{ width: "15.55", height: "11.65px", background: "#7C99B6" }}
+        src={newIcon}
+      />
 
-            <ListItemIcon onClick={() => setOpenCollapse(!openCollapse)}>
-              {">"}
-            </ListItemIcon>
-          </ListItemButton>
-        </ListItem>
-      </List>
-      <Collapse in={openCollapse}>
-        <List
-          sx={{
-            width: 216.72,
-            height: 44.46,
-            background: "#F5F8FA",
-            border: "1px solid #CBD6E2",
-            borderRadius: "2px",
-          }}
-        >
-          {array.map((listElem) => (
-            <ListItem>
-              <Box>
-                <FormControl component="fieldset">
-                  <FormGroup aria-label="position" row>
-                    <FormControlLabel
-                      value="start"
-                      control={<Checkbox color="primary" />}
-                    />
-                  </FormGroup>
-                </FormControl>
-                {/* <ListItemIcon>{}</ListItemIcon> */}
-              </Box>
+      <ListItemText
+        sx={{ width: "89.47", height: "22.78px" }}
+        primary={"All contacts"}
+      />
 
-              <ListItemButton>
-                <ListItemText primary={listElem} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Collapse>
+      <Box sx={{ width: "18.27", height: "11.24px" }}>
+        <ListItemIcon onClick={() => setOpenCollapse(!openCollapse)}>
+          {">"}
+        </ListItemIcon>
+      </Box>
+
+      <Box>
+        <Collapse in={openCollapse}>
+          <List
+            sx={{
+              width: 216.72,
+              height: 44.46,
+              background: "#F5F8FA",
+              border: "1px solid #CBD6E2",
+              borderRadius: "2px",
+            }}
+          >
+            {array.map((listElem) => (
+              <ListItem>
+                <Box>
+                  <FormControl component="fieldset">
+                    <FormGroup aria-label="position" row>
+                      <FormControlLabel
+                        value="start"
+                        control={<Checkbox color="primary" />}
+                      />
+                    </FormGroup>
+                  </FormControl>
+                  {/* <ListItemIcon>{}</ListItemIcon> */}
+                </Box>
+
+                <ListItemButton>
+                  <ListItemText primary={listElem} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Collapse>
+      </Box>
     </Box>
   );
 };
