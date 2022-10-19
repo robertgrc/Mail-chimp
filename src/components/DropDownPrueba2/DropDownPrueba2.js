aa80bf4670ec2ec7ddc199f9dc2a94bb035ace34
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-10-19 18:50:13 -0400

    refactoring the dropdown

--- src/components/DropDownPrueba2/DropDownPrueba2.js
+++ src/components/DropDownPrueba2/DropDownPrueba2.js
@@ -8,17 +8,20 @@ import {
   FormControl,
   FormControlLabel,
   FormGroup,
+  IconButton,
   List,
   ListItem,
   ListItemButton,
   ListItemIcon,
   //ListItemIcon,
   ListItemText,
+  MenuItem,
 } from "@mui/material";
 //import { useDispatch } from "react-redux";
 //import Icono1 from "../../assets/Dropdown/Icono1.jpg";
 //import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
-import newIcon from "../../assets/Dropdown/newIcon.jpg";
+import newIcon from "../../assets/Dropdown/newIcon.svg";
+import Polygon2 from "../../assets/Dropdown/Polygon2.svg";
 
 const array = ["Family", "Work Friends", "Another Label"];
 
@@ -38,59 +41,72 @@ const DropDownPrueba = () => {
         border: "1px solid #CBD6E2",
         borderRadius: "2px",
         display: "flex",
-        flexWrap: "wrap",
+        justifyContent: "space-between",
       }}
     >
       {/* <Box sx={{ width: "15.55", height: "11.65px" }}>{"<"}</Box> */}
-      <Avatar
-        sx={{ width: "15.55", height: "11.65px", background: "#7C99B6" }}
-        src={newIcon}
-      />
+      <MenuItem sx={{ display: "flex" }}>
+        <IconButton>
+          <Avatar
+            sx={{ width: "15.55px", height: "11.65px" }}
+            //sx={{ width: "15.55", height: "11.65px", background: "#7C99B6" }}
+            src={newIcon}
+          />
+        </IconButton>
+        <ListItemText
+          sx={{
+            fontFamily: "Noto Sans",
+            fontStyle: "normal",
+            fontWeight: "400",
+            fontSize: "16px",
+            lineHeight: "22px",
+            color: "#33475B",
+          }}
+          primary={"All contacts"}
+        />
+        <Avatar
+          sx={{
+            width: "16.27px",
+            height: "10.24px",
+            borderRadius: "1px",
 
-      <ListItemText
-        sx={{ width: "89.47", height: "22.78px" }}
-        primary={"All contacts"}
-      />
+            //transform: "rotate(180deg)",
+          }}
+          onClick={() => setOpenCollapse(!openCollapse)}
+          src={Polygon2}
+        />
+      </MenuItem>
+      <Collapse in={openCollapse}>
+        <List
+          sx={{
+            width: 216.72,
+            height: 44.46,
+            background: "#F5F8FA",
+            border: "1px solid #CBD6E2",
+            borderRadius: "2px",
+          }}
+        >
+          {array.map((listElem) => (
+            <ListItem>
+              <Box>
+                <FormControl component="fieldset">
+                  <FormGroup aria-label="position" row>
+                    <FormControlLabel
+                      value="start"
+                      control={<Checkbox color="primary" />}
+                    />
+                  </FormGroup>
+                </FormControl>
+                {/* <ListItemIcon>{}</ListItemIcon> */}
+              </Box>
 
-      <Box sx={{ width: "18.27", height: "11.24px" }}>
-        <ListItemIcon onClick={() => setOpenCollapse(!openCollapse)}>
-          {">"}
-        </ListItemIcon>
-      </Box>
-
-      <Box>
-        <Collapse in={openCollapse}>
-          <List
-            sx={{
-              width: 216.72,
-              height: 44.46,
-              background: "#F5F8FA",
-              border: "1px solid #CBD6E2",
-              borderRadius: "2px",
-            }}
-          >
-            {array.map((listElem) => (
-              <ListItem>
-                <Box>
-                  <FormControl component="fieldset">
-                    <FormGroup aria-label="position" row>
-                      <FormControlLabel
-                        value="start"
-                        control={<Checkbox color="primary" />}
-                      />
-                    </FormGroup>
-                  </FormControl>
-                  {/* <ListItemIcon>{}</ListItemIcon> */}
-                </Box>
-
-                <ListItemButton>
-                  <ListItemText primary={listElem} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-        </Collapse>
-      </Box>
+              <ListItemButton>
+                <ListItemText primary={listElem} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Collapse>
     </Box>
   );
 };
---
+++ src/components/NewDropDown/NewDropDown.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import {
+  Avatar,
+  //Avatar,
+  Box,
+  Checkbox,
+  Collapse,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  //ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+//import { useDispatch } from "react-redux";
+//import Icono1 from "../../assets/Dropdown/Icono1.jpg";
+//import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import newIcon from "../../assets/Dropdown/newIcon.jpg";
+
+const array = ["Family", "Work Friends", "Another Label"];
+
+const NewDropDown = () => {
+  const [openCollapse, setOpenCollapse] = useState(false);
+
+  //const dispatch = useDispatch();
+
+  return (
+    <Box
+      sx={{
+        width: "216.72px",
+        height: "44.46px",
+        marginTop: "24px",
+        marginLeft: "33px",
+        background: "#F5F8FA",
+        border: "1px solid #CBD6E2",
+        borderRadius: "2px",
+        display: "flex",
+        flexWrap: "wrap",
+      }}
+    >
+      {/* <Box sx={{ width: "15.55", height: "11.65px" }}>{"<"}</Box> */}
+      <Avatar
+        sx={{ width: "15.55", height: "11.65px", background: "#7C99B6" }}
+        src={newIcon}
+      />
+
+      <ListItemText
+        sx={{ width: "89.47", height: "22.78px" }}
+        primary={"All contacts"}
+      />
+
+      <Box sx={{ width: "18.27", height: "11.24px" }}>
+        <ListItemIcon onClick={() => setOpenCollapse(!openCollapse)}>
+          {">"}
+        </ListItemIcon>
+      </Box>
+
+      <Box>
+        <Collapse in={openCollapse}>
+          <List
+            sx={{
+              width: 216.72,
+              height: 44.46,
+              background: "#F5F8FA",
+              border: "1px solid #CBD6E2",
+              borderRadius: "2px",
+            }}
+          >
+            {array.map((listElem) => (
+              <ListItem>
+                <Box>
+                  <FormControl component="fieldset">
+                    <FormGroup aria-label="position" row>
+                      <FormControlLabel
+                        value="start"
+                        control={<Checkbox color="primary" />}
+                      />
+                    </FormGroup>
+                  </FormControl>
+                  {/* <ListItemIcon>{}</ListItemIcon> */}
+                </Box>
+
+                <ListItemButton>
+                  <ListItemText primary={listElem} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Collapse>
+      </Box>
+    </Box>
+  );
+};
+
+export default NewDropDown;
